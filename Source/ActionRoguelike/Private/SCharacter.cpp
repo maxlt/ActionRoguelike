fce74778d13ca09b2f7145b0faef4c973e70b2cd
mdmx <xibaryon2500@gmail.com>
2023-02-08 22:06:27 +0800

    Dark Souls-style player character control

--- Source/ActionRoguelike/Private/SCharacter.cpp
+++ Source/ActionRoguelike/Private/SCharacter.cpp
@@ -3,6 +3,9 @@
 #include "SCharacter.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "DrawDebugHelpers.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 
 // Sets default values
 ASCharacter::ASCharacter()
@@ -13,6 +16,11 @@ ASCharacter::ASCharacter()
 	SpringArmComp->SetupAttachment(GetRootComponent());
 	CameraComp = CreateDefaultSubobject<UCameraComponent>("Camera Component");
 	CameraComp->SetupAttachment(SpringArmComp);
+
+	// Set following flags to enable Dark Souls-style character control
+	bUseControllerRotationYaw = false;
+	SpringArmComp->bUsePawnControlRotation = true;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
 }
 
 // Called when the game starts or when spawned
@@ -24,7 +32,17 @@ void ASCharacter::BeginPlay()
 
 void ASCharacter::MoveForward(float Value)
 {
-	AddMovementInput(GetActorForwardVector(), Value);
+	// Make this character moves forward/reverses along the Controller's forward axis
+	FRotator ControlRot = GetControlRotation();
+	AddMovementInput(ControlRot.Vector(), Value);
+}
+
+void ASCharacter::MoveRight(float Value)
+{
+	// Make this character turns to the left/right of the Controller's forward axis
+	FRotator ControlRot = GetControlRotation();
+	ControlRot.Roll = ControlRot.Pitch = 0.f;
+	AddMovementInput(UKismetMathLibrary::GetRightVector(ControlRot), Value);
 }
 
 // Called every frame
@@ -32,6 +50,14 @@ void ASCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	// Render a yellow arrow that indicates the direction this character is facing i.e. direction of character's x-axis
+	// in world space
+	const FVector LineStart = GetActorLocation() + GetActorRightVector() * 100.f;
+	const FVector LineEnd = GetActorForwardVector() * 100.f + LineStart;
+	DrawDebugDirectionalArrow(GetWorld(), LineStart, LineEnd, 100.f, FColor::Yellow, false, 0.f, 0u, 5.f);
+
+	// Render an RGB axis-triplet that indicates the rotation of the Controller
+	DrawDebugCoordinateSystem(GetWorld(), LineStart, GetControlRotation(), 50.f, false, 0.f, 0u, 1.f);
 }
 
 // Called to bind functionality to input
@@ -40,6 +66,8 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 	PlayerInputComponent->BindAxis("MoveForward", this, &ASCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ASCharacter::MoveRight);
 	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 }
 
