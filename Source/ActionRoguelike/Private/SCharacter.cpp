4525732896ee1f37759b495936dd987da6a6d156
mdmx <xibaryon2500@gmail.com>
2023-03-05 15:52:22 +0800

    Improve magic projectile
    - added the instigator i.e. pawn
    - added emitter fx
    - destroy projectile upon hit

--- Source/ActionRoguelike/Private/SCharacter.cpp
+++ Source/ActionRoguelike/Private/SCharacter.cpp
@@ -68,6 +68,7 @@ void ASCharacter::PrimaryAttack_TimeElapsed()
 
 	FActorSpawnParameters SpawnParams{};
 	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	SpawnParams.Instigator = this;
 
 	GetWorld()->SpawnActor<AActor>(ProjectileClass, SpawnTM, SpawnParams);
 }
